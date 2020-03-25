39ed3318d32bf4ea2d90e46035f8f41bc5bff0b2
AndreiCondurache98 <48328905+AndreiCondurache98@users.noreply.github.com>
2020-03-25 16:14:14 +0200

    .

---
+++ Laboratoare 1-5/Laborator1/Info.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laborator1
+{
+    public class Info
+    {
+        // delegate pentru eveniment
+        public delegate void InfoDelegate(object obj);
+        // event ce foloseste delegate InfoDelegate
+        public event InfoDelegate InfoChanged;
+        // data membru
+        object _obiect;
+        public object SetInfo
+        {
+            set
+            {
+                _obiect = value;
+                // genereaza eveniment
+                InfoChanged(_obiect);
+            }
+        }
+    }
+}
---
+++ Laboratoare 1-5/Laborator1/InfoDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laborator1
+{
+    public class InfoDisplay
+    {
+        public void InfoChanged(object obj)
+        {
+            Console.WriteLine("Info changed");
+        }
+    }
+}
