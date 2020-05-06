85355fa92993dff96f2d87af0fbc757826a961be
AndreiCondurache98 <48328905+AndreiCondurache98@users.noreply.github.com>
2020-05-06 15:36:59 +0300

    .

---
+++ Asp_Condurache_Andrei_Rp/Asp_Condurache_Andrei_Rp/Data/Asp_Condurache_Andrei_RpContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Asp_Condurache_Andrei_Rp.Models;
+
+namespace Asp_Condurache_Andrei_Rp.Data
+{
+    public class Asp_Condurache_Andrei_RpContext : DbContext
+    {
+        public Asp_Condurache_Andrei_RpContext (DbContextOptions<Asp_Condurache_Andrei_RpContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Asp_Condurache_Andrei_Rp.Models.Movie> Movie { get; set; }
+    }
+}
