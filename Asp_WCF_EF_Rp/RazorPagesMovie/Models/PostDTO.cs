fe2b81506c3036a228f244dbd71e96e8c631cedc
AndreiCondurache98 <48328905+AndreiCondurache98@users.noreply.github.com>
2020-05-13 15:54:21 +0300

    .

---
+++ Asp_WCF_EF_Rp/RazorPagesMovie/Models/PostDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovie.Models
+{
+    [DataContract]
+    public partial class PostDTO
+    {
+        public PostDTO()
+        {
+            this.Comments = new List<CommentDTO>(); //new HashSet<Comment>();
+        }
+        [DataMember]
+        public int PostId { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+        [DataMember]
+        public string Domain { get; set; }
+        [DataMember]
+        public System.DateTime Date { get; set; }
+        [DataMember]
+        public virtual List<CommentDTO> Comments { get; set; }
+    }
+
+}
